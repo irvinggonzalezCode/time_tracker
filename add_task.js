add6bd57c65099504a16328d2da6882123d7bca9
Irving_GX92 <chainsawloco@yahoo.com>
2026-08-19 23:45:31 -0400

    added some buttons and add task logic

---
+++ add_task.js
@@ -0,0 +1,8 @@
+const modal = document.getElementById("task-modal");
+const openModalButton = document.getElementById("add-task");
+const closeModalButton = document.getElementById("close-modal");
+
+function addTask() {
+    // Implementation for adding a new task
+    modal.style.display = "block";
+}
